fcbda7c38b7d254f4fe42eb3df69f8a369654906
Patrick Browne <pt.browne@gmail.com>
2020-04-19 21:08:46 +0200

    chore: Remove template comments

--- src/index.js
+++ src/index.js
@@ -12,15 +12,7 @@ const format = require('date-fns/format')
 const path = require('path')
 
 const request = requestFactory({
-  // The debug mode shows all the details about HTTP requests and responses. Very useful for
-  // debugging but very verbose. This is why it is commented out by default
-  // debug: true,
-  // Activates [cheerio](https://cheerio.js.org/) parsing on each page
-  //cheerio: true,
-  // If cheerio is activated do not forget to deactivate json parsing (which is activated by
-  // default in cozy-konnector-libs
   json: true,
-  // This allows request-promise to keep cookies between requests
   jar: true
 })
 
@@ -29,11 +21,6 @@ const baseUrl = 'https://www.famileo.com'
 
 module.exports = new BaseKonnector(start)
 
-// The start function is run by the BaseKonnector instance only when it got all the account
-// information (fields). When you run this connector yourself in "standalone" mode or "dev" mode,
-// the account information come from ./konnector-dev-config.json file
-// cozyParameters are static parameters, independents from the account. Most often, it can be a
-// secret api key.
 async function start(fields, cozyParameters) {
   log('info', 'Authenticating ...')
   if (cozyParameters) log('debug', 'Found COZY_PARAMETERS')
@@ -54,28 +41,15 @@ async function start(fields, cozyParameters) {
   }
 }
 
-// This shows authentication using the [signin function](https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#module_signin)
-// even if this in another domain here, but it works as an example
 function authenticate(username, password) {
   return signin({
     url: `${baseUrl}/login`,
     formSelector: 'form',
     formData: { _username: username, _password: password },
-    // The validate function will check if the login request was a success. Every website has a
-    // different way to respond: HTTP status code, error message in HTML ($), HTTP redirection
-    // (fullResponse.request.uri.href)...
-    validate: (statusCode, $, fullResponse) => {
-      log(
-        'debug',
-        fullResponse.request.uri.href,
-        'not used here but should be useful for other connectors'
-      )
-      // The login in toscrape.com always works except when no password is set
+    validate: (statusCode, $) => {
       if ($(`a[href='/logout']`).length === 1) {
         return true
       } else {
-        // cozy-konnector-libs has its own logging function which format these logs with colors in
-        // standalone and dev mode and as JSON in production mode
         log('error', $('.ui-pnotify-text').text())
         return false
       }
@@ -111,7 +85,6 @@ async function fetchGazettes(family, fields) {
   })
 
   if (!response || !response.gazettes.length) {
-    // error
     log('debug', response, 'no gazettes found')
     return
   }
@@ -134,10 +107,7 @@ function parseGazettes(docs) {
     )}.pdf`,
     vendor: VENDOR,
     metadata: {
-      // It can be interesting to add the date of import. This is not mandatory but may be
-      // useful for debugging or data migration
       importDate: new Date(),
-      // Document version, useful for migration after change of document structure
       version: 1
     }
   }))
@@ -194,10 +164,7 @@ function parseContacts(docs) {
     birthday: doc.birthday.split(' ')[0],
     email: [{ address: doc.email, type: 'home', label: 'Personnel' }],
     metadata: {
-      // It can be interesting to add the date of import. This is not mandatory but may be
-      // useful for debugging or data migration
       importDate: new Date(),
-      // Document version, useful for migration after change of document structure
       version: 1,
       [VENDOR]: {
         id: doc.id
@@ -211,20 +178,14 @@ function buildContactGroups(family) {
     {
       name: 'Famille',
       metadata: {
-        // It can be interesting to add the date of import. This is not mandatory but may be
-        // useful for debugging or data migration
         importDate: new Date(),
-        // Document version, useful for migration after change of document structure
         version: 1
       }
     },
     {
       name: `Famille de ${family.pad_name}`,
       metadata: {
-        // It can be interesting to add the date of import. This is not mandatory but may be
-        // useful for debugging or data migration
         importDate: new Date(),
-        // Document version, useful for migration after change of document structure
         version: 1
       }
     }
